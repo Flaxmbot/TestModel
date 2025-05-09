cf59e754c7e2d2d558e390623b344382ebc8703d
Devrat Kuntal <rdxdevbhai76@gmail.com>
2025-05-09 12:49:30 +0530

    API integration with Render

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -115,7 +115,7 @@ export default function SpaceStationDetection() {
       const formData = new FormData()
       formData.append("image", imageFile)
 
-      const response = await fetch("http://127.0.0.1:8000/check-missing", {
+      const response = await fetch("https://testmodel-sw32.onrender.com/check-missing", {
         method: "POST",
         body: formData,
       })
@@ -179,7 +179,7 @@ export default function SpaceStationDetection() {
       const formData = new FormData()
       formData.append("image", imageFile)
 
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://testmodel-sw32.onrender.com/predict", {
         method: "POST",
         body: formData,
       })
