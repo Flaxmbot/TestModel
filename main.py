acad354839f7c4c881c7d88bae7870a7f669a750
Devrat Kuntal <rdxdevbhai76@gmail.com>
2025-05-11 19:12:40 +0530

    CORS issue resolved

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # adjust for your frontend origin
+    allow_origins=["https://code-offenders.vercel.app/"],  # adjust for your frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
